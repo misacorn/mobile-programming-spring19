4b01a9d3fe03d2a39313dd3fb33e5eae8c5e4a45
Misa Corn <dieuhuongngo@gmail.com>
2019-03-02 01:34:57 +0200

    react map requires full address

--- find-address/App.js
+++ find-address/App.js
@@ -1,71 +1,69 @@
 import React from "react";
 import {
+  StyleSheet,
   View,
   TextInput,
   Button,
-  StyleSheet,
   KeyboardAvoidingView
 } from "react-native";
-import { Location, MapView } from "expo";
+import { Location, Permissions, MapView } from "expo";
 
 export default class App extends React.Component {
   state = {
-    location: null,
+    address: "",
     latitude: 60.200692,
-    longitude: 24.934302
+    longitude: 24.934302,
+    behavior: "position"
   };
 
-  inputChange = e => {
-    this.setState({ location: e.target.value });
+  getAddress = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permission to access location was denied"
+      });
+    } else {
+      const address = JSON.stringify(this.state.address);
+      let result = await Location.geocodeAsync(address);
+      let latitude = result[0].latitude;
+      let longitude = result[0].longitude;
+      this.setState({
+        latitude,
+        longitude
+      });
+    }
   };
-
-  findAddress = async () => {
-    const location = JSON.stringify(this.state.location);
-    const locationResult = await Location.geocodeAsync(location);
-    const latitude = locationResult[0].latitude;
-    const longitude = locationResult[0].longitude;
-    this.setState({ latitude, longitude });
-  };
-
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          style={{ flex: 4 }}
-          initialRegion={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.0322,
-            longitudeDelta: 0.0221
-          }}
-        >
-          <MapView.Marker
-            coordinate={{
+        <KeyboardAvoidingView behavior={this.state.behavior}>
+          <MapView
+            style={{ width: 500, height: 800 }}
+            region={{
               latitude: this.state.latitude,
-              longitude: this.state.longitude
+              longitude: this.state.longitude,
+              latitudeDelta: 0.04,
+              longitudeDelta: 0.05
             }}
-            title={this.state.location}
-          />
-        </MapView>
-        >
-        <View style={styles.submitBox}>
-          <TextInput
-            style={{
-              width: 200,
-              borderColor: "gray",
-              borderWidth: 1,
-              height: 40
-            }}
-            onChange={this.inputChange}
-            placeholder="Type your address"
-          />
-          <Button
-            style={styles.button}
-            onPress={this.findAddress}
-            title=" Show "
-          />
-        </View>
-        <KeyboardAvoidingView />
+          >
+            <MapView.Marker
+              coordinate={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude
+              }}
+              title={this.state.address}
+            />
+          </MapView>
+
+          <View style={styles.box}>
+            <TextInput
+              style={{ height: 40, width: 200 }}
+              placeholder="Type your address"
+              onChangeText={address => this.setState({ address })}
+            />
+            <Button title=" Find " onPress={this.getAddress} />
+          </View>
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -77,7 +75,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
-  button: {
-    justifyContent: "space-between"
+  map: {
+    flex: 5
+  },
+  box: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    textAlign: "center"
   }
 });
