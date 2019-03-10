bbfaf070d10b519aef8e34cbb1ded8241a1d3a23
Misa <dieuhuongngo@gmail.com>
2019-03-10 17:56:23 +0200

    installed firebase

--- sqlite/shoppingList/App.js
+++ sqlite/shoppingList/App.js
@@ -9,8 +9,20 @@ import {
   Alert
 } from "react-native";
 import Expo, { SQLite } from "expo";
+import * as firebase from "firebase";
 
 const db = SQLite.openDatabase("shoppingList.db");
+const config = {
+  apiKey: "AIzaSyBnpyPh5aw-OktXYKdcZvdrCusP1x5AeDA",
+  authDomain: "shoppinglist-sqlite.firebaseapp.com",
+  databaseURL: "https://shoppinglist-sqlite.firebaseio.com",
+  projectId: "shoppinglist-sqlite",
+  storageBucket: "shoppinglist-sqlite.appspot.com",
+  messagingSenderId: "335532346331"
+};
+
+firebase.initializeApp(config);
+firebase.database().ref("items/");
 
 export default class App extends React.Component {
   constructor(props) {
@@ -20,26 +32,41 @@ export default class App extends React.Component {
 
   componentDidMount() {
     // Create course table
-    db.transaction(tx => {
-      tx.executeSql(
-        "create table if not exists list (id integer primary key not null, product text, amount text);"
-      );
-    });
-    this.updateList();
+    // db.transaction(tx => {
+    //   tx.executeSql(
+    //     "create table if not exists list (id integer primary key not null, product text, amount text);"
+    //   );
+    // });
+    // this.updateList();
+    firebase
+      .database()
+      .ref("items/")
+      .on("value", snapshot => {
+        const data = snapshot.val();
+        const items = Object.values(data);
+        this.setState({ items });
+      });
   }
 
   // Save course
   saveItem = () => {
-    db.transaction(
-      tx => {
-        tx.executeSql(
-          "insert into list (product, amount) values (?, ?)",
-          [this.state.product, this.state.amount]
-        );
-      },
-      null,
-      this.updateList
-    );
+    // db.transaction(
+    //   tx => {
+    //     tx.executeSql(
+    //       "insert into list (product, amount) values (?, ?)",
+    //       [this.state.product, this.state.amount]
+    //     );
+    //   },
+    //   null,
+    //   this.updateList
+    // );
+    firebase
+      .database()
+      .ref("items/")
+      .push({
+        product: this.state.product,
+        amount: this.state.amount
+      });
   };
 
   // Update courselist
@@ -87,7 +114,7 @@ export default class App extends React.Component {
           placeholder="Product"
           style={{
             marginTop: 30,
-            fontSize: 18,
+            fontSize: 14,
             width: 200,
             borderColor: "gray",
             borderWidth: 1
@@ -102,7 +129,7 @@ export default class App extends React.Component {
           style={{
             marginTop: 5,
             marginBottom: 5,
-            fontSize: 18,
+            fontSize: 14,
             width: 200,
             borderColor: "gray",
             borderWidth: 1
@@ -111,7 +138,13 @@ export default class App extends React.Component {
           value={this.state.amount}
         />
         <Button onPress={this.saveItem} title="Save" />
-        <Text style={{ marginTop: 30, fontSize: 20 }}>
+        <Text
+          style={{
+            marginTop: 20,
+            fontSize: 20,
+            marginBottom: 10
+          }}
+        >
           Shopping List
         </Text>
         <FlatList
@@ -119,11 +152,11 @@ export default class App extends React.Component {
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <View style={styles.listcontainer}>
-              <Text style={{ fontSize: 18 }}>
+              <Text style={{ fontSize: 14 }}>
                 {item.product}, {item.amount}{" "}
               </Text>
               <Text
-                style={{ fontSize: 18, color: "#0000ff" }}
+                style={{ fontSize: 14, color: "#0000ff" }}
                 onPress={() => this.deleteItem(item.id)}
               >
                 done
